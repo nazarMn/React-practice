f6cdd888f5522bc037eac7b4404f588c3f02c5f2
MarkivNazar <69341802+nazarMn@users.noreply.github.com>
2024-09-10 20:58:39 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div>
+            <div className="headerConteiner">
+                <div className="headerBox">
+                <h2>/Creator IT Academy</h2>
+                <p>Школа програмування та робототехніки</p>
+                </div>
+            </div>
+        </div>
+    )
+}
