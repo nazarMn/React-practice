ba966a0b176d3046411d3faef857a41c753015a4
MarkivNazar <nazarmn2008@gmail.com>
2024-09-14 11:15:00 +0300

    2

--- src/components/CardConteiner/Card.jsx
+++ src/components/CardConteiner/Card.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Card.css'
+<link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic" rel="stylesheet" />
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
@@ -16,8 +17,17 @@ const Courses = () => {
       {courses.map((course) => (
         <div key={course.id} className="course-card">
           <img src={course.pic} alt={course.title} className="course-pic" />
+          <div className="coursesBox">
+            <div className="coursesBoxTop">
           <h2>{course.title}</h2>
+          </div>
+          <div className="coursesBoxCenter">
           <p>{course.descriptoion}</p>
+          </div>
+          <div className="coursesBoxBottom">
+          <p>{course.age}</p>
+          </div>
+          </div>
         </div>
       ))}
     </div>
